af34f6c870606ec89ac6cc084032340fb607e8c1
Adarsh Singh <Adarsh.Singh@watchguard.com>
2024-11-07 14:04:59 +0530

    taking inputs using signals into our components and how to read their values.

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,28 +1,20 @@
-import { Component, signal ,computed} from '@angular/core';
-import { DUMMY_USERS } from '../dummy-users';
-
-const randomIndex = Math.floor(Math.random()*DUMMY_USERS.length)
+import { Component,Input,input,computed }  from '@angular/core';
 
 @Component({
   selector: 'app-user',
   standalone: true,
   imports: [],
   templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+  styleUrl: './user.component.css' 
 })
 export class UserComponent {  
+  // @Input({required:true}) avatar!: string; 
+  // @Input({required:true}) name!: string;
+  avatar = input.required<string>();
+  name = input.required<string>();
+  imagePath = computed(()=>{
+    return 'assets/users/' + this.avatar();
+  })
 
-  //this is a signal - whenever any change is detected in it, angular 
-  //will be notified and all the usages as well as the UI will be updated.
-
-  selectedUser = signal(DUMMY_USERS[randomIndex]);
-  imagePath = computed(()=> 'assets/users/' + this.selectedUser().avatar)
-
-  //this function is triggered on selecting user
-  
-  onSelectUser(){
-    const randomIndex = Math.floor(Math.random()*DUMMY_USERS.length)
-    // this is how we update a signal
-    this.selectedUser.set(DUMMY_USERS[randomIndex])
-  }
+  onSelectUser(){}
 }
