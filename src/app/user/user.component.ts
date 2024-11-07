8f8cf8d811336e5d79f02cd7bd8d7d80dc5541e9
Adarsh Singh <Adarsh.Singh@watchguard.com>
2024-11-07 09:56:20 +0530

    introduced signals, now the user profile and the name changes whenever the user clicks the profile image

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, signal ,computed} from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users';
+
+const randomIndex = Math.floor(Math.random()*DUMMY_USERS.length)
 
 @Component({
   selector: 'app-user',
@@ -7,6 +10,19 @@ import { Component } from '@angular/core';
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
-export class UserComponent {
+export class UserComponent {  
 
+  //this is a signal - whenever any change is detected in it, angular 
+  //will be notified and all the usages as well as the UI will be updated.
+
+  selectedUser = signal(DUMMY_USERS[randomIndex]);
+  imagePath = computed(()=> 'assets/users/' + this.selectedUser().avatar)
+
+  //this function is triggered on selecting user
+  
+  onSelectUser(){
+    const randomIndex = Math.floor(Math.random()*DUMMY_USERS.length)
+    // this is how we update a signal
+    this.selectedUser.set(DUMMY_USERS[randomIndex])
+  }
 }
