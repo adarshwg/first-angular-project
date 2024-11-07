eb7001215d14092357921413c195170ccea8f5c5
Adarsh Singh <Adarsh.Singh@watchguard.com>
2024-11-07 16:12:30 +0530

    emitting the event of clicking the button from the component to the main app, and updating the ui accordingly, using the output function (previos - decorator)

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -4,6 +4,7 @@ import {
   input,
   computed,
   Output,
+  output,
   EventEmitter,
 } from '@angular/core';
 
@@ -18,7 +19,7 @@ export class UserComponent {
   @Input({ required: true }) id!: string;
   @Input({ required: true }) avatar!: string;
   @Input({ required: true }) name!: string;
-  @Output() select = new EventEmitter();
+  select = output<string>()
   get imagePath() {
     return 'assets/users/' + this.avatar;
   }
